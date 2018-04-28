48773669a11b5fa3bcac3bfc45cdbbf1fabd2045
2no <kazunori.ninomiya@gmail.com>
2018-04-29 00:44:46 +0900

    refactoring.

---
+++ README.md
@@ -0,0 +1,17 @@
+otohime
+========
+
+## Configure
+
+```json
+{
+  "lightLevel": {
+    "threshold": 0.03,
+    "checkInterval": 1000
+  },
+  "slack": {
+    "token": "Slack token",
+    "webhookURL": "Slack webhook URL"
+  }
+}
+```
--- index.js
+++ index.js
@@ -1,37 +1,60 @@
 'use strict';
 
-const tessel = require('tessel');
-const ambientlib = require('ambient-attx4');
-const ambient = ambientlib.use(tessel.port['A']);
+const DEFAULT_CONSOLE_LEVEL = 2;
 
-ambient.on('error', console.error);
+const config         = require('./config.json');
+const tessel         = require('tessel');
+const tesselAV       = require('tessel-av');
+const ambient        = require('ambient-attx4').use(tessel.port['A']);
+const Slack          = require('pico-slack');
+const consoleToSlack = require('console-to-slack');
+const playArgs       = [require('path').join(__dirname, 'sound.mp3'), '-r'];
 
-ambient.on('ready', () => {
-  const config = require('./config.json');
-  const Slack = require('pico-slack');
+const SlackUserPresence = {
+  AWAY: 'away',
+  AUTO: 'auto',
+};
 
-  Slack.connect(config.slack.token)
-    .then(() => {
-      let currentPresence = 'away';
+let currentPresence = SlackUserPresence.AWAY;
+let player          = null;
 
-      setInterval(() => {
-        ambient.getLightLevel((err, lightData) => {
-          if (err) {
-            console.error(err);
-            process.exit(1);
-          }
-    
-          const lightLevel = lightData.toFixed(8);
-          console.log(`Light level: ${lightLevel}`);
-    
-          const presence = lightLevel >= config.lightLevel.threshold ? 'auto' : 'away';
-          if (currentPresence !== presence) {
-            currentPresence = presence;
-            Slack.api('users.setPresence', {presence})
-              .then(console.log).catch(console.error);
-          }
-        });
-      }, config.lightLevel.checkInterval);
-    })
-    .catch(console.error);
-});
+const consoleLevel = config.console.level || DEFAULT_CONSOLE_LEVEL;
+consoleToSlack.init(config.slack.webhookURL, consoleLevel);
+
+try {
+  player = new tesselAV.Player();
+} catch (e) {
+  console.warn('スピーカーとの接続に失敗しました。');
+}
+
+ambient.on('error', console.error).on('ready', handleAmbientReady);
+
+function handleAmbientReady() {
+  Slack.connect(config.slack.token).catch(console.error).then(() => {
+    const interval = config.lightLevel.checkInterval;
+    setInterval(() => ambient.getLightLevel(handleAmbientLightLebel), interval);
+  });
+}
+
+function handleAmbientLightLebel(error, lightData) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const lightLevel = lightData.toFixed(8);
+  console.log(`Light level: ${lightLevel}`);
+
+  const presence = lightLevel >= config.lightLevel.threshold
+    ? SlackUserPresence.AUTO : SlackUserPresence.AWAY;
+  if (currentPresence === presence) {
+    return;
+  }
+
+  currentPresence = presence;
+  if (player !== null) {
+    presence === SlackUserPresence.AUTO ? player.play(playArgs) : player.stop();
+  }
+
+  Slack.api('users.setPresence', {presence}).catch(console.error).then(console.log);
+}
